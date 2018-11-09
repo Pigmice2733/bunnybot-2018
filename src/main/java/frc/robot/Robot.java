1e6076b1484d8b38b3130315df3179d91e1fe6f1
name <a;lsdkjf>
2018-11-08 16:49:06 -0800

    Implemented arcade drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,28 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.drivetrain.Drivetrain;
 
 public class Robot extends TimedRobot {
+    private Drivetrain drivetrain;
+    private Joystick joy;
 
     public void robotInit() {
+        WPI_TalonSRX leftDrive = new WPI_TalonSRX(0);
+        WPI_TalonSRX rightDrive = new WPI_TalonSRX(2);
+
+        WPI_TalonSRX leftFollower = new WPI_TalonSRX(1);
+        leftFollower.set(ControlMode.Follower, leftDrive.getDeviceID());
+
+        WPI_TalonSRX rightFollower = new WPI_TalonSRX(3);
+        rightFollower.set(ControlMode.Follower, rightDrive.getDeviceID());
+
+        drivetrain = new Drivetrain(leftDrive, rightDrive);
+        joy = new Joystick(0);
 
         setPeriod(0.02);
     }
@@ -22,6 +40,7 @@ public class Robot extends TimedRobot {
     }
 
     public void teleopPeriodic() {
+        drivetrain.arcadeDrive(-joy.getY(), joy.getX());
     }
 
     public void disabledInit() {
